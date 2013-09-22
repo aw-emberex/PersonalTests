7471dc72f53849398305588bed8d7e783db5d4b2
Alex Wait <alex.wait@emberex.com>
2013-09-21 22:17:42 -0700

    Project commit.

---
+++ CalendarTest/CalendarTest/CalendarTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CalendarTest
+//
+//  Created by Alex Wait on 9/21/13.
+//  Copyright (c) 2013 AlexWait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
